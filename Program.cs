2b4ac3fafe116caeb60982fec38018d1fe82a30a
shreenidhi45 <75909459+shreenidhi45@users.noreply.github.com>
2023-08-30 23:38:29 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿namespace CRUDOperations
+{
+    internal class Program
+    {
+        
+            
+            static List<string> items = new List<string>();
+
+            // Create
+            static void AddItem(string item)
+            {
+                items.Add(item);
+            }
+
+            // Read
+            static void GetItems()
+            {
+                foreach (string item in items)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            // Update 
+            static void EditItem(int index, string item)
+            {
+                items[index] = item;
+            }
+
+            // Delete
+            static void RemoveItem(int index)
+            {
+                items.RemoveAt(index);
+            }
+
+            static void Main(string[] args)
+            {
+                // Create 
+                AddItem("Item 1");
+                AddItem("Item 2");
+
+                // Read
+                GetItems();
+
+                // Update
+                EditItem(0, "Rohit");
+
+                // Delete
+                RemoveItem(1);
+
+                // Read again
+                GetItems();
+            }
+        }
+    }
+    
